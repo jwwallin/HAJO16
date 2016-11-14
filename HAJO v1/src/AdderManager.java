b1e2cda64e087d54b98436fcb2f35b93df4c3b28
jwwallin <jussi.w.wallin@gmail.com>
2016-11-14 13:51:01 +0200

    Untested code
    EDIT: tested no crashes

--- HAJO v1/src/AdderManager.java
+++ HAJO v1/src/AdderManager.java
@@ -5,7 +5,24 @@ public class AdderManager {
 
 	public static void main(String[] args) {
 		ObjectInputStream in;
-		Socket s = new Socket();
+		DatagramSocket s;
+
+		InetAddress addr;
+		byte[] addrBytes = {(byte) 127, (byte) 0, (byte) 0, (byte) 1};
+		
+		try {
+
+			s = new DatagramSocket();
+			addr = InetAddress.getByAddress(addrBytes);
+			byte[] buff = { (byte) 15555 };
+			DatagramPacket p = new DatagramPacket(buff, buff.length);
+			s.connect(addr, 15554);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
