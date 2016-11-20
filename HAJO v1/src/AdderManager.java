40c957b5db4abd6ec104b71d4b46df1ad7e2b03e
jwwallin <jussi.w.wallin@gmail.com>
2016-11-20 15:01:42 +0200

    Added test for UDP send: Packet sent successfully

--- HAJO v1/src/AdderManager.java
+++ HAJO v1/src/AdderManager.java
@@ -4,24 +4,34 @@ import java.net.*;
 public class AdderManager {
 
 	public static void main(String[] args) {
-		ObjectInputStream in;
-		DatagramSocket s;
+		ObjectInputStream in = null;
+		DatagramSocket s = null;
 
 		InetAddress addr;
 		byte[] addrBytes = {(byte) 127, (byte) 0, (byte) 0, (byte) 1};
 		
 		try {
 
-			s = new DatagramSocket();
+			s = new DatagramSocket(15555);
 			addr = InetAddress.getByAddress(addrBytes);
-			byte[] buff = { (byte) 15555 };
+			byte[] buff = { (byte) 1024 };
 			DatagramPacket p = new DatagramPacket(buff, buff.length);
 			s.connect(addr, 15554);
+			s.send(p);
+			
+			
 			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		
+		
+		
+		
+		if (s != null)
+			s.disconnect();
 		
 	}
 
