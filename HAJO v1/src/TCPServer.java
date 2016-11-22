259c7181413e8051e9a6ef27c6cd58c78960ed56
Wa1tee <antti.auranen@iki.fi>
2016-11-22 19:49:10 +0200

    Added TCPServer.java and UDPClient.java

---
+++ HAJO v1/src/TCPServer.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+
+//idea kopsattu stackoverflowsta. Pitää testailla ja säätää et sopii meijän ohjelmaan
+public class TCPServer{
+	public TCPServer() throws IOException {
+		new Thread(new Runnable() {
+			public void run() {
+				try{
+					ServerSocket serverS = new ServerSocket(1234);
+					Socket clientS = serverS.accept();
+					long startTime = System.currentTimeMillis();
+					int read;
+					int totalRead = 0;
+					InputStream clientInputStream = clientS.getInputStream();
+					while ((read = clientInputStream.read(buffer)) != -1){
+						totalRead += read;
+					}
+					long endTime = System.currentTimeMillis();
+					System.out.println(totalRead + " bittiä " + (endTime - startTime) + " millisekunnissa.");
+
+				}catch (IOException e){
+				}
+			}
+		}).start();
+	}
+}
---
+++ HAJO v1/src/UDPClient.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.net.*;
+// asiakasluokka, joka lähettää UDP datagrammina TCP-porttinumeron, johon palvelin muodostaa yhteyden
+public class UDPClient{
+	public static void main(String[] args) throws Exception {
+		DatagramSocket s;
+		
+
+	}
+}
+
+//tää on kätsy rivi johonkin väliin (ehkä?)
+//new String(receivePacket.getData(), 0, receivePacket.getLength(), StandardCharsets.UTF_8)
