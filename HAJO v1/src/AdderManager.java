8282b65f234b58e223b5d7d4e347b4d89fc78ae7
jwwallin <jussi.w.wallin@gmail.com>
2016-11-28 13:54:03 +0200

    Successfully receive adder count

--- HAJO v1/src/AdderManager.java
+++ HAJO v1/src/AdderManager.java
@@ -14,13 +14,11 @@ public class AdderManager {
 		// object streams for io
 		ObjectInputStream in = null;
 		ObjectOutputStream out = null;
-
-		// create byte array stream for output
-		final ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
 		
 		//define socket variables
 		DatagramSocket datagramSock = null;
-		ServerSocket tcpSock = null;
+		ServerSocket tcpSockServ = null;
+		Socket tcpSock = null;
 
 		InetAddress addr;
 
@@ -28,27 +26,27 @@ public class AdderManager {
 		int tcpTimeout = 0;
 
 		try {
-			// create new outputstream for
-			out = new ObjectOutputStream(byteArrayOut);
-
 			// prepare TCP socket for usage with ObjectInput and -Output streams
-			tcpSock = new ServerSocket(myTcpPort);
-			tcpSock.setSoTimeout(5000); // 5 second timeout
+			tcpSockServ = new ServerSocket(myTcpPort);
+			tcpSockServ.setSoTimeout(5000); // 5 second timeout
 
 			// create local datagram socket and connect to remote
 			datagramSock = new DatagramSocket(15555);
 			addr = InetAddress.getByName(remoteName);
 			datagramSock.connect(addr, remoteDatagramPort);
 
-			// create packet and send local TCP-port number to WorkDistributor
+			// create packet to send local TCP-port number to WorkDistributor
 			byte[] buff = Integer.toString(myTcpPort).getBytes();
 			DatagramPacket p = new DatagramPacket(buff, buff.length);
-			datagramSock.send(p);
-
+			
+			boolean retry = true;
 			do{
 				try {
+					//send the packet
+					datagramSock.send(p);
+					
 					// wait for TCP connection
-					tcpSock.accept();
+					tcpSock = tcpSockServ.accept();
 					System.out.println("Connection accepted");
 					
 				} catch (SocketTimeoutException e) {
@@ -59,20 +57,65 @@ public class AdderManager {
 						System.exit(0);
 					}
 				} //try
+
+				if (tcpSock == null || !tcpSock.isConnected()) {
+					System.out.println("Connection failed");
+				} else {
+					retry = false;
+				}
 				
-			} while (true);
+			} while (retry);
 			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(0);
 		} //try
+
 		
+		try {
+			//instantiate input and output streams
+			in = new ObjectInputStream(tcpSock.getInputStream());
+			out = new ObjectOutputStream(tcpSock.getOutputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		//fetch adder count
+		int adderCount = 0;
+		try {
+			if (in.available() > 0)
+				adderCount = in.readInt();
+				System.out.println("Requested adder count: " + adderCount);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 
+		boolean run = true;
+		while (run) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				run = false;
+			}
+		}
+
+
+		// close all open sockets
 		if (datagramSock != null)
 			datagramSock.disconnect();
+		
+		if (tcpSockServ != null)
+			try {
+				tcpSockServ.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		
 		if (tcpSock != null)
 			try {
 				tcpSock.close();
