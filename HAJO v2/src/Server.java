b23928685fb818bb821fd7368cb087e6a595617c
jwwallin <jussi.w.wallin@gmail.com>
2017-01-12 13:24:22 +0200

    Created main server class

---
+++ HAJO v2/src/Server.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jussi Wallin, Antti Auranen, Niklas Luomala
+ *
+ */
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
