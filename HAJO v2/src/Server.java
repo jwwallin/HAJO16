710fd364896c4288ffb4630ddfdeeb8d53e2cf0b
jwwallin <jussi.w.wallin@gmail.com>
2017-01-13 11:30:17 +0200

    Continue testing how RMI works

--- HAJO v2/src/Server.java
+++ HAJO v2/src/Server.java
@@ -6,14 +6,40 @@
  * @author Jussi Wallin, Antti Auranen, Niklas Luomala
  *
  */
-public class Server {
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+        
+
+public class Server implements TicTacToe {
+	
+	public Server () {};
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			Server srv = new Server();
+			TicTacToe stub = (TicTacToe) UnicastRemoteObject.exportObject(srv, 0);
 
+			Registry registry = LocateRegistry.getRegistry(1099);
+			registry.bind("TicTacToe", stub);
+			
+			System.err.println("Debug: Server ready.");
+		} catch (Exception e) {
+			System.err.println("Debug: Error: "+ e.toString());
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public int registerPlayer() throws RemoteException {
+		System.err.println("Debug: Registered player");
+		return -1;
 	}
 
 }
