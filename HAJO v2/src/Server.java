3939d41a24829de0c9e4b81ce675e1adf2dd13a9
Wa1tee <antti.auranen@iki.fi>
2017-01-31 22:25:52 +0200

    comments to Server.java

--- HAJO v2/src/Server.java
+++ HAJO v2/src/Server.java
@@ -15,14 +15,15 @@ public class Server {
 	 */
 	public static void main(String[] args) {
 		try {
+			// Creates the server object
 			TicTacToe srv = new TicTacToeSrv();
-
+			// Name the RMI call using java.rmi.Naming library
 			Naming.rebind("rmi://localhost:15000/TicTacToe", srv);
-			
+			// Print message to server console output
 			System.err.println("Debug: Server ready.");
 			
-			
 		} catch (Exception e) {
+			// Print error message to server console output 
 			System.err.println("Debug: Error: "+ e.toString());
 			e.printStackTrace();
 		}
